5f52ba7c81ef2bf6ca09225e8771ad37f9adeb53
SaltwaterC <saltwaterc@gmail.com>
2015-10-30 12:16:39 +0000

    Fixed CentOS 5.1 lack of sudo -E by upgrading to 1.7.2p1.

--- packer/centos-5.1-x86_64/scripts/base.sh
+++ packer/centos-5.1-x86_64/scripts/base.sh
@@ -1,2 +1,11 @@
 yum -y groupinstall Core
 yum -y install gcc make kernel-headers kernel-devel-`uname -r`
+
+# update sudo - vagrant expects sudo -E to work properly
+if ((1<<32)); then
+  arch=x86_64
+else
+  arch=i386
+fi
+
+rpm -Uvh "http://vault.centos.org/5.10/os/${arch}/CentOS/sudo-1.7.2p1-28.el5.${arch}.rpm"

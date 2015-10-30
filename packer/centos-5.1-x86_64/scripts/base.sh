0e8af749f3effa4797283fd76a5f5216886e8df7
SaltwaterC <saltwaterc@gmail.com>
2015-10-30 01:29:59 +0000

    Cleaned up after the previous whitespace happy editor.

--- packer/centos-5.1-x86_64/scripts/base.sh
+++ packer/centos-5.1-x86_64/scripts/base.sh
@@ -1,3 +1,2 @@
 yum -y groupinstall Core
 yum -y install gcc make kernel-headers kernel-devel-`uname -r`
-
--- packer/centos-5.1-x86_64/scripts/cleanup.sh
+++ packer/centos-5.1-x86_64/scripts/cleanup.sh
@@ -2,4 +2,3 @@ yum -y erase gcc make kernel-headers kernel-devel-`uname -r`
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
-

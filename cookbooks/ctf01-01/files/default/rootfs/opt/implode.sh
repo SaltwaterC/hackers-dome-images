ec8dea9aeedd574e81a45fcda486a90a9d391a1d
SaltwaterC <saltwaterc@gmail.com>
2015-11-09 18:22:52 +0000

    Improved the implode script for ctf01-01.

--- cookbooks/ctf01-01/files/default/rootfs/opt/implode.sh
+++ cookbooks/ctf01-01/files/default/rootfs/opt/implode.sh
@@ -9,4 +9,11 @@ rm -vf /home/vagrant/install.sh
 rm -vrf /tmp/vagrant-chef
 rm -vrf /var/chef
 
+rm -vf /var/lib/dhclient/dhclient-eth0.leases
+rm -vf /var/lib/dhclient/dhclient-eth1.leases
+chattr -i /etc/resolv.conf
+echo 'PEERDNS=yes' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+
+rm -vf /etc/ssh/ssh_host_*
+
 rm -vf /opt/implode.sh

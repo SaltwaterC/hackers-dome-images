1cbc20fbccb9c363be7ca4b3d4d77488c859f7f7
SaltwaterC <saltwaterc@gmail.com>
2016-09-02 01:01:27 +0100

    Add multi-stage setup for ctf02-01.

--- cookbooks/ctf02-01/recipes/default.rb
+++ cookbooks/ctf02-01/recipes/default.rb
@@ -1,14 +1,3 @@
-%w(
-  /var/mail/root
-  /root/.bash_history
-  /home/ubuntu/.bash_history
-  /var/log/auth.log
-  /var/log/apache2/access.log
-  /var/log/apache2/error.log
-  /var/log/apache2/other_vhosts_access.log
-  /etc/udev/rules.d/70-persistent-net.rules
-).each do |fi|
-  file fi do
-    action :delete
-  end
-end
+# only used by Test Kitchen
+include_recipe 'ctf02-01::stage1'
+include_recipe 'ctf02-01::stage2'
---
+++ cookbooks/ctf02-01/recipes/stage2.rb
@@ -0,0 +1,22 @@
+%w(
+  /var/mail/root
+  /root/.bash_history
+  /home/ubuntu/.bash_history
+  /var/log/auth.log
+  /var/log/apache2/access.log
+  /var/log/apache2/error.log
+  /var/log/apache2/other_vhosts_access.log
+  /etc/udev/rules.d/70-persistent-net.rules
+).each do |fi|
+  file fi do
+    action :delete
+  end
+end
+
+pkg = %w(
+  apache2
+  build-essential
+  htop
+  mysql-server
+)
+package pkg
